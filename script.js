0b33a02f3eeebf7230284d3a01a1fad1f1790ea8
Fernando Valenzuela <fvp314@gmail.com>
2022-06-06 17:33:00 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const hotel1 = {
+    name: "Disney Galactic Starcruiser",
+    rooms: 100,
+    stars: "⭐⭐⭐⭐",
+    costPerNight: 10000
+}
+
+const hotel2 = {
+    name: "Mos Eisley Superdeeelux",
+    rooms: 22,
+    stars: "⭐",
+    costPerNight: 59
+}
+
+function NewRepublicHolidayHotels(data) {
+  this.name = data.name
+  this.rooms = data.rooms
+  this.stars = data.stars
+  this.costPerNight = data.costPerNight
+}
+const starCruiser = new NewRepublicHolidayHotels(hotel1)
+const superDeelux = new NewRepublicHolidayHotels(hotel2)
+
+console.log(starCruiser.stars)
+console.log(superDeelux)
